7cbff302b618fbc6ced3f40edee12d220f8a7b2d
ezekielsansing-hash <ezekiel.sansing@gmail.com>
2026-01-26 19:35:27 -0500

    set up basic no2vance app

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>No2Vance</h1>
+      <p className={styles.description}>A minimal Next.js app</p>
+    </main>
+  )
+}
